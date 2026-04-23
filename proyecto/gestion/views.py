400ba184fc78f4250021a45ee07a2c50ca58adfe
NicolMunoz012 <nicol.munoz@campusucc.edu.co>
2026-04-23 11:36:54 -0500

    feat: agregando avances

--- proyecto/gestion/views.py
+++ proyecto/gestion/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, DeleteView
+from django.urls import reverse_lazy
+from .models import Autor, Libro
+from .forms import AutorForm, LibroForm
 
-# Create your views here.
+# Vistas Basadas en Clases (CBV) para Autor
+class AutorListView(ListView):
+    model = Autor
+    template_name = 'gestion/autor_list.html'
+    context_object_name = 'autores'
+
+class AutorDeleteView(DeleteView):
+    model = Autor
+    template_name = 'gestion/autor_confirm_delete.html'
+    success_url = reverse_lazy('autor_list')
+
+# Vistas Basadas en Funciones (FBV) para Autor
+def autor_create(request):
+    if request.method == 'POST':
+        form = AutorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('autor_list')
+    else:
+        form = AutorForm()
+    return render(request, 'gestion/autor_create.html', {'form': form})
+
+def autor_update(request, pk):
+    autor = get_object_or_404(Autor, pk=pk)
+    if request.method == 'POST':
+        form = AutorForm(request.POST, instance=autor)
+        if form.is_valid():
+            form.save()
+            return redirect('autor_list')
+    else:
+        form = AutorForm(instance=autor)
+    return render(request, 'gestion/autor_update.html', {'form': form})
